2e0a363834c48c293c5dd4474d95371a251b4dc8
StewartSmyth <Stewart.E.Smyth@gmail.com>
2024-11-30 16:52:49 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [helloWorldText, setHelloWorld] = useState(0);
+
+  useEffect(() => {
+    fetch('/hello').then(res=>res.json()).then(data=>{
+      setHelloWorld(data.Test);
+    });
+
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>HelloWorldText: {helloWorldText}</p>
       </header>
     </div>
   );
