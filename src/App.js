b2256626cf18f4a52ebbf1afff7ab92c3c4eb1e1
StewartSmyth <Stewart.E.Smyth@gmail.com>
2024-12-01 16:56:07 +0000

    Add Table which Hooks from /recipes api call

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 
 function App() {
-  const [helloWorldText, setHelloWorld] = useState(0);
+  const [recipeList, setRecipeList] = useState([]);
 
   useEffect(() => {
-    fetch('/hello').then(res=>res.json()).then(data=>{
-      setHelloWorld(data.Test);
-    });
-
+    fetch('/recipes')
+    .then(res=>res.json())
+    .then(data=>setRecipeList(data));
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>HelloWorldText: {helloWorldText}</p>
-      </header>
+      <h2>Recipe Table</h2>
+      <table className="RecipeTable">
+        <tr>
+          <th>Test</th>
+          <th>Test2</th>
+          <th>ingredients</th>
+        </tr>
+        {recipeList.map(recipe=> (
+          <tr key={recipe.id}>
+            <td>{recipe.Name}</td> 
+            <td>{recipe.Test2}</td>
+            <td>{recipe.ingredients}</td>
+          </tr>
+        ))}
+      </table>
+
     </div>
   );
 }
