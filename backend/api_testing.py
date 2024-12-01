0e0034e0cb0fd0bcc3feccbd9b699dcb3386ebde
StewartSmyth <Stewart.E.Smyth@gmail.com>
2024-12-01 14:46:16 +0000

    Add API testing

---
+++ backend/api_testing.py
@@ -0,0 +1,12 @@
+import unittest
+from api import app
+
+class APITesting(unittest.TestCase):
+
+    def setUp(self):
+        self.client = app.test_client()
+    
+    def indexTest(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Index Page', response.data)
